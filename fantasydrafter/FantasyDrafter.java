84b7b5c779c48763a2ad6d3d7620667701b155bf
Christopher Bachicha <cbachich@gmail.com>
2012-08-07 08:42:47 -0600

    Initial commit that includes basic HMI Includes:  - Basic table with error checking when start is selected  - A console to display errors and processes In work:  - Locking the table when Drafto is running

---
+++ fantasydrafter/FantasyDrafter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fantasydrafter;
+
+import fantasydrafter.hmi.DraftDisplay;
+
+/**
+ *
+ * @author cbachich
+ */
+public class FantasyDrafter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DraftDisplay display = new DraftDisplay();
+        display.setVisible(true);
+    }
+}
